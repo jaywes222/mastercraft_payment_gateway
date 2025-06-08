2cae62332b9bab957fb2277ed22ae06781fd0bcd
jaywes222 <wesleyjay202@gmail.com>
2025-06-08 16:18:01 +0300

    {FEAT} Payment Model

--- payment_gateway/payments/models.py
+++ payment_gateway/payments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+    PAYMENT_STATUS = [
+        ('pending', 'Pending'),
+        ('success', 'Success'),
+        ('failed', 'Failed'),
+    ]
+    
+    reference = models.CharField(max_length=100, unique=True)
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=PAYMENT_STATUS, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.customer_name} - {self.reference} ({self.status})"
